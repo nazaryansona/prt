26a5f1c3cff2f475631af6318f0a35950fbb49c3
nazaryansona <nazaryan05sona@gmail.com>
2025-09-16 16:37:40 +0200

    Add Armenian Hello World

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf(a() + b());
+        System.out.println("Բարեւ աշխարհ!");
     }
 
     private static String a() {
